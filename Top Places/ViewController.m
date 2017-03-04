174a12b0705c8fa9d9563de84d1fbe93e3fd642c
Mester, Zsolt <zsolt.mester@misys.com>
2017-03-04 22:51:10 +0100

    Add a Flickr fetcher utility.

--- Top Places/ViewController.m
+++ Top Places/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "FlickrFetcher.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,10 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+	NSData *topPlacesJSON = [NSData dataWithContentsOfURL:[FlickrFetcher URLforTopPlaces]];
+	NSDictionary *topPlaces = [NSJSONSerialization JSONObjectWithData:topPlacesJSON options:0 error:NULL];
+	NSLog(@"%@", topPlaces.description);
 }
 
 
