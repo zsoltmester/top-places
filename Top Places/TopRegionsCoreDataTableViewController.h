36abe62c94d83d6e04ab697da6ff6abdb9ec4d23
Mester, Zsolt <zsolt.mester@misys.com>
2017-03-09 11:45:24 +0100

    Add top regions screen skeleton.

---
+++ Top Places/TopRegionsCoreDataTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopRegionsCoreDataTableViewController.h
+//  Top Places
+//
+//  Created by Zsolt Mester on 2017. 03. 09..
+//  Copyright © 2017. Zsolt Mester. All rights reserved.
+//
+
+#import "CoreDataTableViewController.h"
+
+@interface TopRegionsCoreDataTableViewController : CoreDataTableViewController
+
+@end
---
+++ Top Places/TopRegionsCoreDataTableViewController.m
@@ -0,0 +1,17 @@
+//
+//  TopRegionsCoreDataTableViewController.m
+//  Top Places
+//
+//  Created by Zsolt Mester on 2017. 03. 09..
+//  Copyright © 2017. Zsolt Mester. All rights reserved.
+//
+
+#import "TopRegionsCoreDataTableViewController.h"
+
+@interface TopRegionsCoreDataTableViewController ()
+
+@end
+
+@implementation TopRegionsCoreDataTableViewController
+
+@end
